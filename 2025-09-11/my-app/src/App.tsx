b3495766a66124e99f62953d60105cc3794ddc07
fredtare <tarenomm@tlu.ee>
2025-09-22 20:22:20 +0300

    feat: add ThemeProvider use to Contacts page and all others

--- 2025-09-11/my-app/src/App.tsx
+++ 2025-09-11/my-app/src/App.tsx
@@ -3,11 +3,13 @@ import Counter from "./Components/Counter"
 import Dice from "./Components/Dice"
 import Introduction from "./Components/Introduction"
 
+//iga uus component on oma uue instanciga ja mäluga.
 function App() {
   return(  //okei chatgpt andis selle meetodi mulle.
     <>
-    <Introduction />
+    
     <Counter />
+    <Introduction />
     <Dice />
     </>
   ) 
