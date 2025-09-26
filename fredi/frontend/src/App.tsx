35603b13754c661b1c77c0828f83e300deb0319d
fredtare <tarenomm@tlu.ee>
2025-09-26 17:34:30 +0300

    feat: add PropDrilling and Context examples

--- fredi/frontend/src/App.tsx
+++ fredi/frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import Cats from './components/Cats'
+import Context from './components/Context'
+import PropDrilling from './components/PropdDrilling'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return( <Cats/>
+  return( <>
+ <Cats/>
+ Kontekst näide:
+  <Context/>
+  Propdrill Näide
 
+ </>
   )
 }
 
